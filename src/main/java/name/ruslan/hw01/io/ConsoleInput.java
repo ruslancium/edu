2822a43c88319f3cc8e64b0956c56469e2706ec4
ruslan1979 <ruslan.arifullin@gmail.com>
2017-02-21 17:41:17 +0300

    1) Делаю новую структуру пакетов; 2) Делаю функционал программы более гибким

---
+++ src/main/java/name/ruslan/hw01/io/ConsoleInput.java
@@ -0,0 +1,26 @@
+package name.ruslan.hw01.io;
+
+import java.util.Scanner;
+
+/**
+ * Created by Ruslan_Arifullin on 2/21/2017.
+ * Класс для считывания символов из консоли
+ */
+public class ConsoleInput {
+
+    public static int getInteger() throws CustomException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("PLease input an integer > ");
+
+        int x = 0;
+
+        if (sc.hasNextInt()) {
+            x = sc.nextInt();
+        } else {
+            sc.next();
+            throw new CustomException("The value should be integer. Please try again");
+        }
+
+        return x;
+    }
+}
