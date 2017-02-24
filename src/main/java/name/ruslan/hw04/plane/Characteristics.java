1e8d6a7c0c75808e5a928730c57cf367db16cb12
ruslan1979 <ruslan.arifullin@gmail.com>
2017-02-24 18:06:54 +0300

    Начал писать авиакомпанию

---
+++ src/main/java/name/ruslan/hw04/plane/Characteristics.java
@@ -0,0 +1,16 @@
+package name.ruslan.hw04.plane;
+
+/**
+ * Created by Ruslan_Arifullin on 2/24/2017.
+ */
+public class Characteristics {
+    private Company company;
+    private String name;
+    private int consumption;
+    private int Capacity;
+
+
+
+
+
+}
