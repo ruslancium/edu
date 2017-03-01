02e430e37937f8fed501e3a8eb74b32c3f91ff22
ruslancium <petrusha.wilsonenko@gmail.com>
2017-03-01 17:24:00 +0300

    Классная работа от 1 марта

---
+++ src/main/java/by/epam/jmp/less_class0103/Test.java
@@ -0,0 +1,10 @@
+package by.epam.jmp.less_class0103;
+
+/**
+ * Created by Ruslan_Arifullin on 3/1/2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+    }
+}
